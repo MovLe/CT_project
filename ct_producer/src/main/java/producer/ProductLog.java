0b7281c82c6e839d2529c248935667448767e27c
Movle <richard.xiu.520@gmail.com>
2020-10-06 09:57:05 +0800

    电信项目 修改信息

--- ct_producer/src/main/java/producer/ProductLog.java
+++ ct_producer/src/main/java/producer/ProductLog.java
@@ -56,31 +56,31 @@ public class ProductLog {
         phoneList.add("15490732767");
         phoneList.add("15064972307");
 
-        phoneNameMap.put("15369468720", "李雁");
-        phoneNameMap.put("19920860202", "卫艺");
-        phoneNameMap.put("18411925860", "仰莉");
-        phoneNameMap.put("14473548449", "陶欣悦");
-        phoneNameMap.put("18749966182", "施梅梅");
-        phoneNameMap.put("19379884788", "金虹霖");
-        phoneNameMap.put("19335715448", "魏明艳");
-        phoneNameMap.put("18503558939", "华贞");
-        phoneNameMap.put("13407209608", "华啟倩");
-        phoneNameMap.put("15596505995", "仲采绿");
-        phoneNameMap.put("17519874292", "卫丹");
-        phoneNameMap.put("15178485516", "戚丽红");
-        phoneNameMap.put("19877232369", "何翠柔");
-        phoneNameMap.put("18706287692", "钱溶艳");
-        phoneNameMap.put("18944239644", "钱琳");
-        phoneNameMap.put("17325302007", "缪静欣");
-        phoneNameMap.put("18839074540", "焦秋菊");
-        phoneNameMap.put("19879419704", "吕访琴");
-        phoneNameMap.put("16480981069", "沈丹");
-        phoneNameMap.put("18674257265", "褚美丽");
-        phoneNameMap.put("18302820904", "孙怡");
-        phoneNameMap.put("15133295266", "许婵");
-        phoneNameMap.put("17868457605", "曹红恋");
-        phoneNameMap.put("15490732767", "吕柔");
-        phoneNameMap.put("15064972307", "冯怜云");
+        phoneNameMap.put("15369468720", "李一");
+        phoneNameMap.put("19920860202", "卫一");
+        phoneNameMap.put("18411925860", "仰一");
+        phoneNameMap.put("14473548449", "陶一一");
+        phoneNameMap.put("18749966182", "施一一");
+        phoneNameMap.put("19379884788", "金一一");
+        phoneNameMap.put("19335715448", "魏一一");
+        phoneNameMap.put("18503558939", "华一");
+        phoneNameMap.put("13407209608", "华一一");
+        phoneNameMap.put("15596505995", "仲一一");
+        phoneNameMap.put("17519874292", "卫一");
+        phoneNameMap.put("15178485516", "戚一一");
+        phoneNameMap.put("19877232369", "何一一");
+        phoneNameMap.put("18706287692", "钱一一");
+        phoneNameMap.put("18944239644", "钱二");
+        phoneNameMap.put("17325302007", "缪一一");
+        phoneNameMap.put("18839074540", "焦一一");
+        phoneNameMap.put("19879419704", "吕一一");
+        phoneNameMap.put("16480981069", "沈一");
+        phoneNameMap.put("18674257265", "褚一一");
+        phoneNameMap.put("18302820904", "孙一");
+        phoneNameMap.put("15133295266", "许一");
+        phoneNameMap.put("17868457605", "曹一");
+        phoneNameMap.put("15490732767", "吕一");
+        phoneNameMap.put("15064972307", "冯一");
     }
 
     /**
